947b5f7fc0d97da95f32ecef1c045e182750d079
tillbd <135263600+tillbd@users.noreply.github.com>
2024-04-22 16:14:57 +0300

    Update code.js

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(` January 17 , 2024 00:00:00 
+const newYearTime = new Date(` January 17 , 2025 00:00:00 
 `);
 
 // Update countdown time
